c5bcadc2a3ff5c6b02031c45cf7c5692fc2fb9da
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-03-18 10:18:58 +0100

    remove local storage w/ multer-storage-cloudinary

---
+++ cloudinary/index.js
@@ -0,0 +1,25 @@
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: 'dgd8mcevc',
+    api_key: '795329721125445',
+    api_secret: process.env.CLOUDINARY_SECRET
+
+})
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: 'surf-shop',
+    allowedFormats: ['jpeg', 'jpg', 'png'],
+    filename: function (req, file, cb) {
+        let buf = crypto.randomBytes(16);
+        buf = buf.toString('hex');
+        let uniqFileName = file.originalname.replace(/\.jpeg|\.jpg|\.png/ig, '');
+        uniqFileName += buf;
+        cb(undefined, uniqFileName);
+    }
+});
+
+module.exports = {
+    cloudinary,
+    storage
+}
